978f5c58ed542c46870ac5fe016dcd48d01e277b
sakshi <sanjaykumar@sanjays-MacBook-Air.local>
2019-06-18 05:33:15 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express=require('express');
+var hbs=require('hbs');
+
+ var a=express();
+a.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partial');
+a.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamit',(text)=>{
+  return text.toUpperCase();
+});
+
+
+a.get('/',(req,res)=>{
+   res.render('root.hbs',{
+     pageTitle:'welcomeszzz',
+     welcome:'well well well welcome...leggo'
+   });
+});
+
+a.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'about about ',
+  });
+});
+
+a.get('/bad',(req,res)=>{
+  res.send({
+    errorMessage:'unable to hanlde req'
+  })
+});
+
+a.listen(3000, ()=>{
+  console.log('server is up');
+});
